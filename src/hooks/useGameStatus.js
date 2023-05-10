68acb43bdc98ce0fcc23528ba65675633ebd2aaa
TonyMM55 <tonymmoran@gmail.com>
2023-05-10 21:50:24 +0100

    fixed index.js issue

--- src/hooks/useGameStatus.js
+++ src/hooks/useGameStatus.js
@@ -5,16 +5,16 @@ export const useGameStatus = rowsCleared => {
   const [rows, setRows] = useState(0);
   const [level, setLevel] = useState(0);
 
-  const linePoints = [40, 100, 300, 1200]; //moved from line 8 as i was getting a hooks error
   
   const calcScore = useCallback(() => {
     // We have scores
     
+    const linePoints = [40, 100, 300, 1200]; //moved from line 8 as i was getting a hooks error
     if (rowsCleared > 0) {
       setScore(prev => prev + linePoints[rowsCleared - 1] * (level + 1));
       setRows(prev => prev + rowsCleared);
     }
-  }, [level, linePoints, rowsCleared]);
+  }, [level, rowsCleared]);
 
   useEffect(() => {
     calcScore();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+// import React from 'react';
+// import { createRoot } from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+// createRoot(document.getElementById('root')).render(<App />);
+
 import React from 'react';
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
 createRoot(document.getElementById('root')).render(<App />);
-
